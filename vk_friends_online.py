2e43ddbf99f07fc0dd0b896e2184633dc95ffad5
Your Name <you@example.com>
2016-10-02 11:20:15 +0300

    Запрос логина/пароля пользователя и вывод друзей, кто онлайн

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,59 @@
 import vk
+import argparse
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5651126
 
 
-def get_user_login():
-    pass
+def createParser():
+    parser = argparse.ArgumentParser(usage='%(prog)s [аргументы]',
+                                     description="Вывод друзей в VK, "
+                                                 "кто онлайн, "
+                                                 "с помощью %(prog)s")
+    parser.add_argument('-l', '--login', nargs='?', help="Логин пользователя")
+    parser.add_argument('-p', '--password', nargs='?', help="Пароль пользователя")
+    return parser
 
 
-def get_user_password():
-    pass
+def get_user_login(namespace):
+    login = namespace.login
+    if not login:
+        login = input('Пожалуйста, введите логин пользователя: ')
+    return login
 
 
-def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+def get_user_password(namespace):
+    password = namespace.password
+    if not password:
+        password = input('Пожалуйста, введите пароль пользователя: ')
+    return password
 
 
-def output_friends_to_console(friends_online):
-    pass
+def get_api(login, password):
+    session = vk.AuthSession(app_id=APP_ID, user_login=login,
+                             user_password=password, scope=2)
+    return vk.API(session)
+
+
+def get_online_friends(api):
+    friends_online_ids = api.friends.getOnline()
+    friends_online_info = api.users.get(user_ids=friends_online_ids)
+    return friends_online_info
+
+
+def output_friends_to_console(api,friends_online):
+    print("Список ваших друзей, кто Онлайн:")
+    for friend in friends_online:
+        print(friend['first_name'],friend['last_name'])
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    parser = createParser()
+    namespace = parser.parse_args()
+
+    login = get_user_login(namespace)
+    password = get_user_password(namespace)
+    api = get_api(login, password)
+
+    friends_online = get_online_friends(api)
+    output_friends_to_console(api, friends_online)
